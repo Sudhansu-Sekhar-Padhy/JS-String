f4f02a70de28099c8308059efefc13987afa9490
Sudhansu-Sekhar-Padhy <sidsekhar809@gmail.com>
2022-10-16 00:29:01 +0530

    Multiline strings.js

---
+++ multiline strings.js
@@ -0,0 +1,3 @@
+ //instead of (+) we ca use (\) operator
+let message1 = ' this is a long message '+' that spans accross multiple lines '+' in the code '
+console.log(message1);// this is a long message that spans across multiple lines in the code 
